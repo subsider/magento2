a5f33b3327edfa701e194ca0a7d72c8e4dd3fc20
ccruz <%G0l3t4__$>
2020-02-25 14:19:26 +0100

    Create FirstModule, module.xml and registration file

--- src/app/etc/config.php
+++ src/app/etc/config.php
@@ -224,6 +224,7 @@ return [
         'Klarna_Kp' => 1,
         'Magento_PaypalReCaptcha' => 1,
         'MSP_TwoFactorAuth' => 1,
+        'Simplified_FirstModule' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1,
         'Yotpo_Yotpo' => 1
